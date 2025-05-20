5ac196c4238ba587a9514fbfa39b8640f4f9991a
Nxsh <nathanael.mutua.m@gmail.com>
2025-05-20 20:54:53 +0300

    add 'challenge2: Minutes to seconds' solution
    - add function expression/anonymous function
    - add 'secs' local scope variable
    - add output using string literals

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -4,47 +4,59 @@
 function addition(num1, num2){ //function declaration
     console.log(num1 + num2);
 }
-addition(2, 2);
+addition(3, 5);
 
 let addition2 = function(num1, num2){ //tried function expression
     console.log(num1 + num2);
 }
-addition2(3, 3);
+addition(-6, 9);
 
-let addition3 = (num1, num2) => console.log(num1 + num2); //tried arrow function
+let addition3 = (num1, num2) => console.log(num1 + num2); //tried arrow function, which was the tricky one
 addition3(4, 4);
 
 // -----------------------------------------------------------------------
+console.log("---------------------------------------------------------")
 // Challenge 2: Convert minutes into seconds
 // Write a function called convert that takes an integer minutes and converts it to seconds.
 
-
+//in essence 1min = 60 seconds
+let convert = function(mins){
+    let secs = mins * 60; // used a local scope variable declaration(learnt tdy)
+    console.log(`${mins} minutes = ${secs} seconds`) // used string literals
+}
+convert(5);
+convert(2);
 
 // -----------------------------------------------------------------------
+console.log("---------------------------------------------------------")
 // Challenge 3: Perimeter of a rectangel
 // Create a function that takes length and width and finds the perimeter of a rectangle.
 
 
 
 // -----------------------------------------------------------------------
+console.log("---------------------------------------------------------")
 // Challenge 4: Check Negative
 // Write a function called isNegative that takes a number as it's only argument, the function should return true if the passed number is a negative number and false if the passed number is not a negative number.
 
 
 
 // -----------------------------------------------------------------------
+console.log("---------------------------------------------------------")
 // Challenge 5: Can I Drive
 // Write a function that takes in a person's name and age. If they're 18 or older, return a message like 'Alice is old enough to drive.' If they're younger, return 'Bob is not old enough to drive yet.'
 
 
 
 // -----------------------------------------------------------------------
+console.log("---------------------------------------------------------")
 // Challenge 6: Largest Number
 // Write a function called findLargest(a, b, c) that takes three numbers and returns the largest of the three.
 
 
 
 // -----------------------------------------------------------------------
+console.log("---------------------------------------------------------")
 // Challenge 7: BMI Calculator
 // Write a function called calculateBMI(weight, height) that calculates the Body Mass Index (BMI) using the following formula:
 // BMI = weight / (height * height)
@@ -61,6 +73,7 @@ addition3(4, 4);
 
 
 // -----------------------------------------------------------------------
+console.log("---------------------------------------------------------")
 // Challenge 8: Greeting Based on time
 // Write a function called greetUser(name, hour) that returns a personalized greeting based on the hour of the day.
 // The name is a string representing the user's name.
@@ -74,6 +87,7 @@ addition3(4, 4);
 
 
 // -----------------------------------------------------------------------
+console.log("---------------------------------------------------------")
 // Challenge 9: FizzBuzz
 // Write a function called fizzBuzzCheck(number) that takes a number and returns:
 // "Fizz" if the number is divisible by 3
@@ -84,6 +98,7 @@ addition3(4, 4);
 
 
 // -----------------------------------------------------------------------
+console.log("---------------------------------------------------------")
 // Challenge 10: Perimeter2
 // Write a function that takes a number and returns the perimeter of either a circle or a square. The input will be in the form (letter l, number num) where the letter will be either "s" for square, or "c" for circle, and the number will be the side of the square or the radius of the circle.
 // Use the following formulas:
@@ -92,6 +107,7 @@ addition3(4, 4);
 
 
 // -----------------------------------------------------------------------
+console.log("---------------------------------------------------------")
 // Challenge 11: Sum of Even Numbers
 // Write a function called sumEvenNumbers(n) that takes a number n and returns the sum of all even numbers from 1 up to and including n.
 
@@ -104,18 +120,21 @@ addition3(4, 4);
 
 
 // -----------------------------------------------------------------------
+console.log("---------------------------------------------------------")
 // Challenge 13: Factorial Calculator
 // Write a function called factorial(n) that takes a non-negative integer n and returns the factorial of n — that’s the product of all positive integers from 1 up to n. By definition, factorial of 0 is 1.
 
 
 
 // -----------------------------------------------------------------------
+console.log("---------------------------------------------------------")
 // Challenge 14: Multiple Sum
 // Write a function called sumMultiples(n, divisor) that returns the sum of all numbers from 1 up to n that are divisible by divisor.
 
 
 
 // -----------------------------------------------------------------------
+console.log("---------------------------------------------------------")
 // Challenge 15: Sum of Digits
 // Write a function called sumDigits(num) that takes a positive integer and returns the sum of its digits.
 // Constraints:
