064a4167d386640edc38106b49940c56af730489
Nxsh <nathanael.mutua.m@gmail.com>
2025-05-20 20:21:35 +0300

    add challenge 1: Sum of Two solutions
    - use function declaration[addition()] to find sum of two
    numbers
    - use function expression[addition2()] to find the sum of two
    numbers
    - use arrow function[addition3()] to find the sum of two
    numbers

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -1,33 +1,50 @@
 // Challenge 1: Return the sum of two numbers
 // Create a function called addition that takes two numbers as arguments and returns their sum.
 
+function addition(num1, num2){ //function declaration
+    console.log(num1 + num2);
+}
+addition(2, 2);
 
+let addition2 = function(num1, num2){ //tried function expression
+    console.log(num1 + num2);
+}
+addition2(3, 3);
 
+let addition3 = (num1, num2) => console.log(num1 + num2); //tried arrow function
+addition3(4, 4);
+
+// -----------------------------------------------------------------------
 // Challenge 2: Convert minutes into seconds
 // Write a function called convert that takes an integer minutes and converts it to seconds.
 
 
 
+// -----------------------------------------------------------------------
 // Challenge 3: Perimeter of a rectangel
 // Create a function that takes length and width and finds the perimeter of a rectangle.
 
 
 
+// -----------------------------------------------------------------------
 // Challenge 4: Check Negative
 // Write a function called isNegative that takes a number as it's only argument, the function should return true if the passed number is a negative number and false if the passed number is not a negative number.
 
 
 
+// -----------------------------------------------------------------------
 // Challenge 5: Can I Drive
 // Write a function that takes in a person's name and age. If they're 18 or older, return a message like 'Alice is old enough to drive.' If they're younger, return 'Bob is not old enough to drive yet.'
 
 
 
+// -----------------------------------------------------------------------
 // Challenge 6: Largest Number
 // Write a function called findLargest(a, b, c) that takes three numbers and returns the largest of the three.
 
 
 
+// -----------------------------------------------------------------------
 // Challenge 7: BMI Calculator
 // Write a function called calculateBMI(weight, height) that calculates the Body Mass Index (BMI) using the following formula:
 // BMI = weight / (height * height)
@@ -43,6 +60,7 @@
 
 
 
+// -----------------------------------------------------------------------
 // Challenge 8: Greeting Based on time
 // Write a function called greetUser(name, hour) that returns a personalized greeting based on the hour of the day.
 // The name is a string representing the user's name.
@@ -55,6 +73,7 @@
 
 
 
+// -----------------------------------------------------------------------
 // Challenge 9: FizzBuzz
 // Write a function called fizzBuzzCheck(number) that takes a number and returns:
 // "Fizz" if the number is divisible by 3
@@ -64,6 +83,7 @@
 
 
 
+// -----------------------------------------------------------------------
 // Challenge 10: Perimeter2
 // Write a function that takes a number and returns the perimeter of either a circle or a square. The input will be in the form (letter l, number num) where the letter will be either "s" for square, or "c" for circle, and the number will be the side of the square or the radius of the circle.
 // Use the following formulas:
@@ -71,26 +91,31 @@
 
 
 
+// -----------------------------------------------------------------------
 // Challenge 11: Sum of Even Numbers
 // Write a function called sumEvenNumbers(n) that takes a number n and returns the sum of all even numbers from 1 up to and including n.
 
 
 
+// -----------------------------------------------------------------------
 // Challenge 12: Multiply by self
 // Write a function called powerUp(num, times) that takes two numbers and returns the result of multiplying num by itself times times using only a loop and arithmetic operators (no Math.pow() allowed).
 
 
 
+// -----------------------------------------------------------------------
 // Challenge 13: Factorial Calculator
 // Write a function called factorial(n) that takes a non-negative integer n and returns the factorial of n — that’s the product of all positive integers from 1 up to n. By definition, factorial of 0 is 1.
 
 
 
+// -----------------------------------------------------------------------
 // Challenge 14: Multiple Sum
 // Write a function called sumMultiples(n, divisor) that returns the sum of all numbers from 1 up to n that are divisible by divisor.
 
 
 
+// -----------------------------------------------------------------------
 // Challenge 15: Sum of Digits
 // Write a function called sumDigits(num) that takes a positive integer and returns the sum of its digits.
 // Constraints:
